835087188a58c01974417a5137acce4e2bb8979c
TusharPatil010010 <tusharp010010@gmail.com>
2020-10-08 09:53:15 +0530

    Added functionality to choose input

--- src/com/capg/Workshop/TicTacToeGame.java
+++ src/com/capg/Workshop/TicTacToeGame.java
@@ -1,17 +1,38 @@
 package com.capg.Workshop;
 
-public class TicTacToeGame {
+import java.util.Scanner;
 
-	public char[] tictactoe = new char[10];
+public class TicTacToeGame {
+	
+	Scanner sc = new Scanner(System.in);
+
+	public String[] tictactoe = new String[10];
 	
 	public void createBoard() {		
 		
 		for(int i = 1; i < tictactoe.length; i++ )
-			tictactoe[i] = ' ';
+			tictactoe[i] = " ";
 	}
 	
+	public String chooseInput() {
+		
+		System.out.println("Enter 'x' or 'o' ");
+		String input = sc.nextLine();
+		
+		if(input.equalsIgnoreCase("x")) {
+			input = "o" ;
+		}
+		else if(input.equalsIgnoreCase("o")){
+			input = "x";
+		}
+		else {
+			System.out.println("Invalid input");
+		}
+		return input;
+	}
 	public static void main(String[] args) {
 		TicTacToeGame obj = new TicTacToeGame();
 		obj.createBoard();
+		obj.chooseInput();
 	}
 }	
